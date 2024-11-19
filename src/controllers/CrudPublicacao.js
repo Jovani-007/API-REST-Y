bc8af6caccd70ef7c94ed8fe46291816a97b6d6f
Jovani-007 <alcaraz.gamis.g@gmail.com>
2024-11-19 15:50:12 -0300

    Fim da aula 19/11; Quarto commit Cristal

--- src/controllers/CrudPublicacao.js
+++ src/controllers/CrudPublicacao.js
@@ -1,1 +1,25 @@
-export default {  };
+import Publicacao from '../models/Publicacoes.js';
+import Usuario from '../models/Usuarios.js';
+
+const criarPublicacao = async (request, response) => {
+    const { publicacao, usuario_id } = request.body;
+    
+    const userExistente = await Usuario.findByPk(usuario_id);
+    if (!publicacao || !usuario_id) {
+        if (!userExistente) {
+            return response.status(400).json({ erro: 'Usuário inexistente' });
+        }
+        return response.status(400).json({ erro: 'Todos os campos são obrigatórios' });
+    }
+
+    try {
+        const novaPublicacao = await Publicacao.create({ 
+            publicacao, usuario_id, qtd_likes: 0
+        });
+        return response.status(201).json(novaPublicacao);
+    } catch (error) {
+        return response.status(500).json({ erro: 'Erro ao criar a publicação' });
+    }
+}
+
+export default { criarPublicacao };
--- src/routes/PublicacaoRouter.js
+++ src/routes/PublicacaoRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 import crud from '../controllers/CrudPublicacao.js';
 
 //ROTAS
+router.post('/', crud.criarPublicacao);
 
 export default router;
