fddc9f47aba7c055f41f8d234c4ade70f200f73d
Jovani-007 <alcaraz.gamis.g@gmail.com>
2024-11-21 12:34:35 -0300

    Sétima commit Garrafa

--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,29 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: '../database.sqlite'
-});
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: '../database.sqlite'
+// });
+
+dotenv.config();
+const sequelize = new Sequelize(
+    process.env.DB_PROD_NAME,
+    process.env.DB_PROD_USER,
+    process.env.DB_PROD_PASSWORD,
+    {
+        host: process.env.DB_PROD_HOST,
+        port: process.env.DB_PROD_PORT,
+        dialect: process.env.DB_PROD_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        logging: false,
+    }
+);
 
 (async () => {
     try {
